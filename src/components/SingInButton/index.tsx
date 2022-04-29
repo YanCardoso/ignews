7954cdb41598392c68a4cc1780621cfbc227d0ed
Yan Cardoso <yancard7@gmail.com>
2022-04-29 05:02:56 -0300

    provider github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,17 +1,27 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-export function SingInButton() {
-  const isUserLoggedIn = true;
+import { signIn, useSession, signOut } from "next-auth/react";
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.btnPrimary}>
+export function SingInButton() {
+  const session = useSession();
+
+  return session.status === "authenticated" ? (
+    <button
+      onClick={() => signOut()}
+      type="button"
+      className={styles.btnPrimary}
+    >
       <FaGithub color="#04b361" />
-      Yan Cardoso
-        <FiX color="#737380" className={styles.closeIcon} />
+      {session.data.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.btnPrimary}>
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.btnPrimary}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
